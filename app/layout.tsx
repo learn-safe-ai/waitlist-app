88d5462ef8e24d43002a8a31c9b10ab40232fab2
im2nguyen <im2nguyen@gmail.com>
2024-10-01 22:13:53 -0700

    add fb pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Toaster } from "@/components/ui/sonner";
 import { Analytics } from "@vercel/analytics/react";
 import { CSPostHogProvider } from "./providers";
 import { GoogleAnalytics } from '@next/third-parties/google';
+import Script from "next/script";
 
 const FigtreeFont = Figtree({ subsets: ["latin"] });
 
@@ -43,6 +44,26 @@ export default function RootLayout({
           <Toaster richColors position="top-center" />
           <Analytics />
           <GoogleAnalytics gaId="G-E2FSXSY97Z" />
+          <Script
+            id="facebook-pixel"
+            strategy="afterInteractive"
+          >
+            {`!function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
+              n.callMethod.apply(n, arguments) : n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1112173830487266');
+            fbq('track', 'PageView');
+            `}
+          </Script>
+          <noscript><img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=1112173830487266&ev=PageView&noscript=1"
+          /></noscript>
+
         </body>
       </CSPostHogProvider>
     </html>
